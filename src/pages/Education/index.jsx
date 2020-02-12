32ef9fcd61b3d4176742703f8e73db8d693a26eb
yvbathia <yvbathia@gmail.com>
2020-02-12 17:38:52 +0530

    Improve education section

--- src/pages/Education/index.jsx
+++ src/pages/Education/index.jsx
@@ -6,16 +6,34 @@ const Education = () => (
     <div className={s.title}>Education</div>
     <div className={s.details}>
       <div className={s.education}>
-        <div className={s.year}>2017</div>
-        <div className={s.uni}>DAIICT</div>
-        <div className={s.degree}>Msc-IT</div>
-        <div className={s.perc}>69%</div>
+        <div className={s.year}>
+          <h2>2017</h2>
+        </div>
+        <div className={s.degree}>
+          <div className={s.name}>Msc-IT from DAIICT</div>
+          <div className={s.perc}>
+            <div className={s.progressWrapper}>
+              <div className={s.progress} style={{ width: "69%" }}>
+                {" "}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={s.education}>
-        <div className={s.year}>2015</div>
-        <div className={s.uni}>Ahmedabad University</div>
-        <div className={s.degree}>BCA</div>
-        <div className={s.perc}>78%</div>
+        <div className={s.year}>
+          <h2>2015</h2>
+        </div>
+        <div className={s.degree}>
+          <div className={s.name}>BCA from Ahmedabad University</div>
+          <div className={s.perc}>
+            <div className={s.progressWrapper}>
+              <div className={s.progress} style={{ width: "78%" }}>
+                {" "}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
