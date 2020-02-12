a1f90764835066c35a861f594e7a1f28fc8b3c67
yvbathia <yvbathia@gmail.com>
2020-02-12 19:08:21 +0530

    Improve Profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import s from "./Header.module.scss";
 import Profile from "../../images/profile.svg";
+import { ReactComponent as Fb } from "../../images/fb.svg";
+import { ReactComponent as Linkedin } from "../../images/linkedin.svg";
+import { ReactComponent as Github } from "../../images/github.svg";
 
 const propType = {
   children: PropTypes.node.isRequired
@@ -19,6 +22,38 @@ const Header = ({ children }) => (
           </div>
           <div className={s.imgContainer}>
             <img className={s.img} src={Profile} alt="img" />
+            <div className={s.title}>
+              <div className={s.name}>
+                <h1>Yash Bathia</h1>
+              </div>
+              <div className={s.subTitle}>Developer</div>
+            </div>
+            <div className={s.social}>
+              <a
+                className={s.icon}
+                href="https://www.facebook.com/yashbathia"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Fb />
+              </a>
+              <a
+                className={s.icon}
+                href="https://www.linkedin.com/in/yash-bathia/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Linkedin />
+              </a>
+              <a
+                className={s.icon}
+                href="https://github.com/yvbathia"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github />
+              </a>
+            </div>
           </div>
         </div>
         <div className={s.details}>
