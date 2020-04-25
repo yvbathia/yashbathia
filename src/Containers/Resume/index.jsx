31c8d62d29ed10616edfd876e74c5490e3322e56
yvbathia <yvbathia@gmail.com>
2020-04-26 00:52:18 +0530

    Init: you're doing great

---
+++ src/Containers/Resume/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./Resume.module.scss";
+
+const Resume = () => {
+  return (
+    <div className={s.root}>
+      <div className={s.s}>Resume</div>
+    </div>
+  );
+};
+
+export default Resume;
