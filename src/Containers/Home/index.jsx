b73d384045b2c73ba07b2df029792a05a77294ac
yvbathia <yvbathia@gmail.com>
2020-04-26 13:31:00 +0530

    Add Social and Resume buttons: you're doing great

--- src/Containers/Home/index.jsx
+++ src/Containers/Home/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout";
 import s from "./Home.module.scss";
 import avatar from "../../images/avatar.jpg";
+import { ReactComponent as FB } from "../../images/fb.svg";
+import { ReactComponent as GitHub } from "../../images/github.svg";
+import { ReactComponent as LinkedIn } from "../../images/linkedin.svg";
+// import { ReactComponent as Twitter } from "../../images/twitter.svg";
+import { CONTACT } from "../../constants/routes";
 
 const Home = () => (
   <Layout>
@@ -9,6 +15,12 @@ const Home = () => (
       <div className={s.about}>
         <div className={s.profile}>
           <img src={avatar} alt="Yash" />
+          <div className={s.social}>
+            <GitHub />
+            <LinkedIn />
+            {/* <Twitter /> */}
+            <FB />
+          </div>
         </div>
         <div className={s.me}>
           <span className={s.eyeBrow}>Front-end developer</span>
@@ -21,6 +33,17 @@ const Home = () => (
             am always excited to do business with like minded people, lets
             discuss over coffee.
           </span>
+          <div className={s.btns}>
+            <a
+              className={s.resume}
+              href="https://drive.google.com/uc?export=download&id=1p5ChdKsN-xgeNqVRmMSRXmnzaQSkK-2g"
+            >
+              Download CV
+            </a>
+            <Link className={s.contact} to={CONTACT}>
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </div>
