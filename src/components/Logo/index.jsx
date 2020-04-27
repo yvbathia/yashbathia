2b0fcc5057ca366de8ce9e710a5e2e801423bc83
yvbathia <yvbathia@gmail.com>
2020-04-27 22:48:02 +0530

    Add home page when click on logo: you're doing great

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import s from "./Logo.module.scss";
+import { Link } from "react-router-dom";
 
 const Logo = () => (
-  <div className={s.root}>
-    <div className={s.symbole}>Y</div>
-    <div className={s.name}>
-      <span className={s.fname}>Yash</span>
-      <span className={s.lname}> Bathia</span>
+  <Link className={s.link} to="/">
+    <div className={s.root}>
+      <div className={s.symbole}>Y</div>
+      <div className={s.name}>
+        <span className={s.fname}>Yash</span>
+        <span className={s.lname}> Bathia</span>
+      </div>
     </div>
-  </div>
+  </Link>
 );
 
 export default Logo;
